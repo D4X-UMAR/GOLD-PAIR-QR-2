d483dac54a19b8c0e1f7b21d3c3bd89980e43ab6
GOOD • BOY <154020288+D4X-UMAR@users.noreply.github.com>
2024-06-28 08:15:11 +0500

    BY • UMAR

---
+++ README.md
@@ -0,0 +1,14 @@
+  <p align="center">
+<a href="https://github.com/MrMasterOfc/SESSION-GENERATE/fork" target="blank"><img align="center" src="https://i.imgur.com/cxaSEWe.png" alt="Deploy bot" height="112" width="310" /></a>
+     
+<img src="https://user-images.githubusercontent.com/73097560/115834477-dbab4500-a447-11eb-908a-139a6edaec5c.gif">
+   <p align="center">
+<a href="https://github.com/MrMasterOfc">
+    <img src="https://telegra.ph/file/c227d87605ffa07c7871c.png" width="700px">
+  </a>
+<img src="https://user-images.githubusercontent.com/73097560/115834477-dbab4500-a447-11eb-908a-139a6edaec5c.gif">
+
+ <details close>
+<summary>HOW TO DEPLOY 👨‍💻 SESSON-GENERATER 👨‍💻</summary>
+   
+
---
+++ qr.js
@@ -0,0 +1,107 @@
+const PastebinAPI = require('pastebin-js'),
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+const {makeid} = require('./id');
+const QRCode = require('qrcode');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+let router = express.Router()
+const pino = require("pino");
+const {
+	default: Maher_Zubair,
+	useMultiFileAuthState,
+	jidNormalizedUser,
+	Browsers,
+	delay,
+	makeInMemoryStore,
+} = require("maher-zubair-baileys");
+
+function removeFile(FilePath) {
+	if (!fs.existsSync(FilePath)) return false;
+	fs.rmSync(FilePath, {
+		recursive: true,
+		force: true
+	})
+};
+const {
+	readFile
+} = require("node:fs/promises")
+router.get('/', async (req, res) => {
+	const id = makeid();
+	async function SIGMA_MD_QR_CODE() {
+		const {
+			state,
+			saveCreds
+		} = await useMultiFileAuthState('./temp/' + id)
+		try {
+			let Qr_Code_By_Maher_Zubair = Maher_Zubair({
+				auth: state,
+				printQRInTerminal: false,
+				logger: pino({
+					level: "silent"
+				}),
+				browser: Browsers.macOS("Desktop"),
+			});
+
+			Qr_Code_By_Maher_Zubair.ev.on('creds.update', saveCreds)
+			Qr_Code_By_Maher_Zubair.ev.on("connection.update", async (s) => {
+				const {
+					connection,
+					lastDisconnect,
+					qr
+				} = s;
+				if (qr) await res.end(await QRCode.toBuffer(qr));
+				if (connection == "open") {
+					await delay(5000);
+					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+					await delay(800);
+				   let b64data = Buffer.from(data).toString('base64');
+				   let session = await Qr_Code_By_Maher_Zubair.sendMessage(Qr_Code_By_Maher_Zubair.user.id, { text: "" + b64data });
+	
+				   let SIGMA_MD_TEXT = `
+┏━━━━━━━━━━━━━━😘
+┃🔰┃ *GOLD-MD CONNECTED* 
+┃🔰┃ MADE BY • UMAR •
+┃🔰┃ _SESSION-ID RECEIVED ✔️_
+┗━━━━━━━━━━━━━━━😘
+*__________________________________*
+*🔰 GOLD-MD REPO 🔰*
+github.com/D4X-UMAR/GOLD-MD
+*__________________________________*
+▬▬▬▬▬▬▬▬▬▬▬▬
+*🔰 GOLD-MD HELP 🔰*
+▬▬▬▬▬▬▬▬▬▬▬▬
+*__________________________________*
+*🔰 WHATSAPP CHANNEL 🔰*
+https://whatsapp.com/channel/0029VabdDuf7DAWylTkTZa0m
+*__________________________________*
+*🔰 WHATSAPP GROUP 🔰*
+https://chat.whatsapp.com/II4CelPFUvIFeK6f8hdJuX
+*__________________________________*
+*_PLEASE GOLD-MD REPO KO STAR LAZMI KARNA_*
+__________________________________`
+	 await Qr_Code_By_Maher_Zubair.sendMessage(Qr_Code_By_Maher_Zubair.user.id,{text:SIGMA_MD_TEXT},{quoted:session})
+
+
+
+					await delay(100);
+					await Qr_Code_By_Maher_Zubair.ws.close();
+					return await removeFile("temp/" + id);
+				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+					await delay(10000);
+					SIGMA_MD_QR_CODE();
+				}
+			});
+		} catch (err) {
+			if (!res.headersSent) {
+				await res.json({
+					code: "Service Unavailable"
+				});
+			}
+			console.log(err);
+			await removeFile("temp/" + id);
+		}
+	}
+	return await SIGMA_MD_QR_CODE()
+});
+module.exports = router
